cacb3eaf6d8a2a98dd3170351fbbe724792ce916
Kaustubh Gupta <kaustubhgupta@jmangroup.com>
2024-02-12 18:11:58 +0530

    In progress

---
+++ src/components/Thankyou.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import './Styles/thankyou.css'
+
+const Thankyou = () => {
+    const [currentSection, setCurrentSection] = useState(1);
+ 
+    useEffect(() => {
+      const section = document.getElementById(`section-${currentSection}`);
+   
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, [currentSection]);
+   
+    const handleOptionClick = () => {
+      setCurrentSection(currentSection === 1 ? 2 : 1);
+    };
+   
+    return (
+      <div>
+        <div id={`section-1`} className="section">
+          <h1>Section One</h1>
+          <div className="options">
+          <button onClick={handleOptionClick}>Next </button>
+        </div>
+        </div>
+        <div id={`section-2`} className="section">
+          <h1>Section Two</h1>
+          <div className="options">
+          <button onClick={handleOptionClick}>Next</button>
+        </div>
+        </div>
+      </div>
+    );
+  };
+
+export default Thankyou
