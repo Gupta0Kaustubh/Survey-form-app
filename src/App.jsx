561779e72ac540649e514a38f3f39f772974d168
Vaibhav S <vaibhavs@jmangroup.com>
2024-02-12 18:15:45 +0530

    admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from './components/home.jsx'
+import Details from './components/admin-details.jsx'
 import './App.css'
 function App() {
   return (
     <>
-    <Home/>
+      <Home/>  
+    <Details/>
     </>
   )
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,24 +3,25 @@ import '../styles/home.css'
 import {Button} from 'react-bootstrap';
 import background from '../assets/home-back.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { IoIosArrowForward } from "react-icons/io";
 export const Home = () => {
   return (
     <div className='container'>
         
 <div className='header'>
-  <span style={{color:"#bfc0c0",marginLeft:"10%"}}>Y</span>our<span style={{color:"#bfc0c0"}}>Survey</span>
+  <span style={{color:"#bfc0c0"}}>Y</span>our<span style={{color:"#bfc0c0"}}>Survey</span>
 </div>
 <div className='survey-quote'>
     <p>
-    It is proved by surveys that happiness does not come from love, wealth, or power but the pursuit of attainable goals.
+    Surveys are like compasses, guiding organizations through the turbulent seas of consumer preferences.
     </p>
 </div>
 <div className='buttons-home'>
-<Button variant="outline-light">Take a Survey</Button>
+<Button variant="outline-light">Take a Survey <IoIosArrowForward /> </Button>
 <Button variant="outline-light">Admin Login</Button>
 </div>
 <img src={background} style={{width:"22%"}}/>
-    </div>
+</div>
   )
 }
 export default Home;
