814b05971f20277e490fe046dfc81c3fb1079a0b
Yogeshwaran G <yogeshwarang@jmangroup.com>
2024-02-13 14:40:43 +0530

    test

--- frontend/src/components/Details.jsx
+++ frontend/src/components/Details.jsx
@@ -1,35 +1,45 @@
-import {React, useEffect, useState} from 'react'
+import { React, useEffect, useState } from 'react'
 import '../styles/admin-details.css'
 import { IoEyeSharp } from "react-icons/io5";
-
+import Accordion from 'react-bootstrap/Accordion';
 export const Details = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      
-        const response = await fetch("http://127.0.0.1:3000/");
-      
-        const data = await response.json();
-        setData(data);
-        console.log(data)
+
+      const response = await fetch("http://127.0.0.1:3000/");
+
+      const data = await response.json();
+      setData(data);
+      console.log(data)
     };
-  
+
     fetchData();
   }, []);
-  
-  
-  
+
+
+
   return (
     <div className='ad-container'>
-    <div className='header-admin'>
-  <span style={{color:"#bfc0c0"}}>A</span>dmin
-</div>
-<main>
-    <div className='users'>
-        <p style={{flexGrow:"1",padding:"10px"}} >User-1 </p>
-       <p style={{flexGrow:"0",fontSize:"30px"}}> <IoEyeSharp /></p>
-    </div>
-</main>
+      <div className='header-admin'>
+        <span style={{ color: "#bfc0c0" }}>A</span>dmin
+      </div>
+      <main>
+      <div className='container' >
+      {data.map(response => (
+      <Accordion  defaultActiveKey="1" style={{width: "80%",paddingBottom:"10px"}}>
+      <Accordion.Item className="border" eventKey="0">
+        <Accordion.Header>{response.response.name}</Accordion.Header>
+        <Accordion.Body>
+          <p>How long have you been using our product/service? </p> <p>{response.response.question1}</p>
+          
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+      ))}
+      </div>
+      </main>
+      
     </div>
   )
 }
